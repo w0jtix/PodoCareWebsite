753bcdfd2b14d883eed786930126557214758f50
Wojtek <wojtix96@gmail.com>
2025-09-05 19:25:38 +0200

    styling adjustments

--- src/components/hooks/usePopupBackgroud.ts
+++ src/components/hooks/usePopupBackgroud.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export const usePopupBackground = (backgroundColor = 'rgba(0, 0, 0, 0.75)') => {
   useEffect(() => {
-    const bodyElement = document.querySelector('.body');
+    const bodyElement = document.body;
     if (bodyElement) {
       bodyElement.classList.add('popup-background-override');
       
