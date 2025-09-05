047f8dcc94d66b8db4ac809edc2f20d02dec12b2
Wojtek <wojtix96@gmail.com>
2025-09-05 20:18:13 +0200

    styling adjustments

--- src/components/hooks/useThemeColor.ts
+++ src/components/hooks/useThemeColor.ts
@@ -1,19 +1,19 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 
 export const useThemeColor = (color: string) => {
-  useEffect(() => {
+  useLayoutEffect(() => {
   const bodyElement = document.body;
   const themeMeta = document.querySelector(
     'meta[name="theme-color"]'
   ) as HTMLMetaElement | null;
 
-  if (!themeMeta) return;
+  if (!bodyElement || !themeMeta) return;
 
+  bodyElement.classList.add("popup-background-override");
   themeMeta.setAttribute("content", color);
-  bodyElement?.classList.add("popup-background-override");
 
   return () => {
-    bodyElement?.classList.remove("popup-background-override");
+    bodyElement.classList.remove("popup-background-override");
     themeMeta.setAttribute("content", "#D7D7D7");
   };
 }, [color]);
