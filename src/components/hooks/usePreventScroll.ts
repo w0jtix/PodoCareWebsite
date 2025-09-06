9e174ad72f31b28e7fbbd05ee39f96d94e5d4910
Wojtek <wojtix96@gmail.com>
2025-09-06 14:44:52 +0200

    styling adjustments

--- src/components/hooks/usePreventScroll.ts
+++ src/components/hooks/usePreventScroll.ts
@@ -1,20 +1,17 @@
 import { useEffect } from "react";
+
 export const usePreventScroll = (popupOpen: boolean) => {
-    useEffect(() => {
-  const body = document.body;
-  if (popupOpen) {
-    const scrollY = window.scrollY;
-    body.style.position = "fixed";
-    body.style.top = `-${scrollY}px`;
-    body.style.left = "0";
-    body.style.right = "0";
-    body.style.overflow = "hidden";
-  } else {
-    const scrollY = body.style.top;
-    body.style.position = "";
-    body.style.top = "";
-    body.style.overflow = "";
-    window.scrollTo(0, parseInt(scrollY || "0") * -1);
-  }
-}, [popupOpen]);
-}
+  useEffect(() => {
+    const body = document.body;
+
+    if (popupOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "";
+    }
+
+    return () => {
+      body.style.overflow = "";
+    };
+  }, [popupOpen]);
+};
